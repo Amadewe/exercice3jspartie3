d7f0934d280eeac91fd537be87e0ce721f01eb6d
Amadewe <dewevre.amandine@gmail.com>
2019-01-25 11:18:57 +0100

    Ajout de l'exerice

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function changeco1orgreen() {
+document.getElementById('text').style.color='green';
+}
+function changecolorred() {
+document.getElementById('text').style.color='red';
+}
+function changecolorblue() {
+document.getElementById('text').style.color='blue';
+}
